280d5d55666e06d3db8b0646d47e649a0b33a3a6
Ludovico O. Russo <ludus.russo@gmail.com>
2017-06-25 16:51:18 +0200

    add function examples

---
+++ function_examples.js
@@ -0,0 +1,17 @@
+// Identity Function
+
+function(x) {
+    return x;
+}
+
+// Double Function
+
+function(x) {
+    return 2*x;
+}
+
+// Math Operation Function
+// performs the sin of the input signal
+function(x) {
+    return Math.sin(x);
+}
